c2a4dec4511b7ff6f44bf3d2860e0d77d6919099
Geo290 <jorgeemmanuel.2003@gmail.com>
2024-07-24 10:51:41 -0600

    Feat: app initialized

--- app/app.js
+++ app/app.js
@@ -1,1 +1,14 @@
+require('dotenv').config();
 
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+const port = 4000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.listen(port, () => {
+    console.log(`SERVER IS LISTENING ON PORT ${port}`);
+});
