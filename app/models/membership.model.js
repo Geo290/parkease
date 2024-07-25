1fb0b4b01b89e3aa80e3a68fda3d171aab9c6232
Geo290 <jorgeemmanuel.2003@gmail.com>
2024-07-24 22:14:35 -0600

    Feat: membership model created

---
+++ app/models/membership.model.js
@@ -0,0 +1,51 @@
+const {Schema, model} = require('mongoose');
+const clientModel = require('./client.model');
+
+const MembershipSchema = new Schema({
+    client: {
+        _id: {
+            type: Schema.Types.ObjectId,
+            ref: clientModel,
+            required: true
+        },
+        names: {
+            type: Schema.Types.String,
+            required: true
+        },
+        lastnames: {
+            type: Schema.Types.String,
+            required: true
+        }
+    },
+    clientVehicle: {
+        plateNumber: {
+            type: Schema.Types.String,
+            required: true
+        },
+        model: {
+            type: Schema.Types.String,
+            required: true
+        },
+        year: {
+            type: Schema.Types.Number,
+            required: true
+        },
+        color: {
+            type: Schema.Types.String,
+            required: true
+        }
+    },
+    lot: {
+        type: Schema.Types.Number,
+        required: true
+    },
+    isActive: {
+        type: Schema.Types.Boolean,
+        reqiured: true
+    }
+},  { timestamps: true }
+);
+
+const membershipModel = model('memberships', MembershipSchema);
+
+module.exports = membershipModel;
