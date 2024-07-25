c99776e1df6796b92db97fa9b25e5325c5f1f5ae
Geo290 <jorgeemmanuel.2003@gmail.com>
2024-07-25 13:11:14 -0600

    Nothing: really, there's nothing but I couldn't undo the canges so I had to push it

---
+++ app/controllers/membership.controller.js
@@ -0,0 +1,11 @@
+const membershipModel = require('../models/membership.model.js');
+
+const membershipCtrl = {};
+
+membershipCtrl.new = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
