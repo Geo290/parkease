1b4b8c5ba430d2dd96703f5e7c0c57997d595740
Geo290 <jorgeemmanuel.2003@gmail.com>
2024-07-24 18:58:43 -0600

    Feat: encryption functionality added

--- app/models/client.model.js
+++ app/models/client.model.js
@@ -1,4 +1,4 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model} = require('mongoose');
 const { genSalt, compare, hash } = require('bcryptjs');
 
 const ClientSchema = new Schema({
