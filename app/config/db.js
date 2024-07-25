0cf6f5bf28091dac43ed4306bf22e1663a9f3832
Geo290 <jorgeemmanuel.2003@gmail.com>
2024-07-25 16:59:03 -0600

    Feat: connection to DB

---
+++ app/config/db.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+
+const { connect, disconnect } = require('mongoose');
+
+const USER = process.env.DB_USERNAME;
+const PASS = process.env.DB_PASSKEY;
+const DB = process.env.DB_NAME;
+const URI = `mongodb+srv://${USER}:${PASS}@cluster0.8puqbr9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const run = async () => {
+    try {
+        await connect(URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            dbName: DB
+        });
+
+        console.log("App connected to server");
+
+    } catch (error) {
+        console.error("Error while connecting to DB: ", error);
+        process.exit(1);
+    }
+}
+
+const stop = async (server) => {
+    server.close(async (error) => {
+        if (error) {
+            console.error("Error while closing the server");
+            process.exit(1);
+        }
+    });
+
+    try {
+        await disconnect();
+        console.log("App successfully disconnected from DB");
+        proces.exit(0);
+
+    } catch (error) {
+        console.error("Error while disconnecting from DB: ", error);
+        process.exit(1);
+    }
+}
+
+module.exports = { run, stop };
