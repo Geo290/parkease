cdbdffd0deb51a6dc69bfd03bb7688120632ddfa
Geo290 <jorgeemmanuel.2003@gmail.com>
2024-07-26 14:02:51 -0600

    Fix: token verifing fixed

--- app/middlewares/verifyToken.js
+++ app/middlewares/verifyToken.js
@@ -1,13 +1,15 @@
-const verifyToken = (req, res, next) => {
-    const token = req.cookie.access_token;
-    let data = null;
+const { verify } = require('jsonwebtoken');
 
+const verifyToken = (req, res, next) => {
+    const token = req.cookies.access_token;
     req.session = { user: null };
 
     try {
         const data = verify(token, process.env.SECRET_JWT_KEY);
-        req.session.user = { data };
-    } catch { }
+        req.session.user = data;
+    } catch (error) {
+        console.error(error);
+    }
 
     next();
 };
