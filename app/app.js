d133932e1449123abc6301810784f6dc59c9ede5
Geo290 <jorgeemmanuel.2003@gmail.com>
2024-07-25 22:13:37 -0600

    Feat: client features integrated; base route

--- app/app.js
+++ app/app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan('dev'));
 
-app.use('api/v1/client', clientRoutes);
+app.use('/api/v1/client', clientRoutes);
 
 run()
     .then(() => {
