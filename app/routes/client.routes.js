34eeec83d8d290a97409ae29c1f54e07bf362e14
Geo290 <jorgeemmanuel.2003@gmail.com>
2024-07-26 14:08:30 -0600

    Feat: logout and verifying token endpoints

--- app/routes/client.routes.js
+++ app/routes/client.routes.js
@@ -7,5 +7,7 @@ const router = Router();
 router
     .post("/signup", clientCtrl.signup)
     .post("/login", clientCtrl.login)
+    .post("/logout", clientCtrl.logout)
+    .get('/protected', verifyToken, clientCtrl.protected)
 
 module.exports = router;
