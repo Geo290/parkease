0adef35c1e82fce883a1b57d335915549878726f
Geo290 <jorgeemmanuel.2003@gmail.com>
2024-08-08 12:44:15 -0600

    Feat: use of headers instead of cookies

--- app/middlewares/verifyAdminToken.js
+++ app/middlewares/verifyAdminToken.js
@@ -1,15 +1,15 @@
 const { verify } = require('jsonwebtoken');
 
 const verifyAdminToken = (req, res, next) => {
-    const token = req.cookies.admin_access_token;
-    req.session = { admin: null };
+    const token = req.headers['admin-access-token'];
+    if (!token) {
+        return res.status(401).json({ message: 'No authorizarion token' });
+    }
 
-    try {
-        const data = verify(token, process.env.SECRET_JWT_KEY);
-        req.session.admin = data;
-    } catch {}
+    const data = verify(token, process.env.SECRET_JWT_KEY);
+    req.admin = data;
 
     next();
-};  
+};
 
 module.exports = verifyAdminToken; // must import this middleware in routes files
--- app/middlewares/verifyUserToken.js
+++ app/middlewares/verifyUserToken.js
@@ -1,13 +1,13 @@
 const { verify } = require('jsonwebtoken');
 
 const verifyUserToken = (req, res, next) => {
-    const token = req.cookies.user_access_token;
-    req.session = { user: null };
+    const token = req.headers['user-access-token'];
+    if (!token) {
+        return res.status(401).json({ message: 'No authorizarion token' });
+    }
 
-    try {
-        const data = verify(token, process.env.SECRET_JWT_KEY);
-        req.session.user = data;
-    } catch {}
+    const data = verify(token, process.env.SECRET_JWT_KEY);
+    req.client = data;
 
     next();
 };
